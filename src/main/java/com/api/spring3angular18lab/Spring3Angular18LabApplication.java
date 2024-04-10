17912f0b673f79d9df88e5dc3cfc8b8e8de0bcca
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-04-10 19:12:23 -0300

    Criação do projeto laboratório - API Rest Springboot 3 e Angular 18

---
+++ src/main/java/com/api/spring3angular18lab/Spring3Angular18LabApplication.java
@@ -0,0 +1,13 @@
+package com.api.spring3angular18lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3Angular18LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3Angular18LabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/spring3angular18lab/Spring3Angular18LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.spring3angular18lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3Angular18LabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
